0c1244435daa431123ed873aac657383de04ddf2
Elsa Fluss <elsafluss@gmail.com>
2021-02-22 19:01:52 -0700

    Display each component.

---
+++ src/components/Display.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Display = () => {
+    return (
+        <Link to="/monster">
+            <div>display list of monsters</div>
+        </Link>
+    )
+}
+
+export default Display
---
+++ src/components/Encounter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Encounter = () => {
+    return (
+        <div>Encounter</div>
+    )
+}
+
+export default Encounter
