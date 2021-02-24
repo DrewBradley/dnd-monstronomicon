24ee80a33a5db14548d9b0f3ae8e02faa0932a02
Drew Bradley <williamandrewb@gmail.com>
2021-02-23 19:46:21 -0700

    Create reducer.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './reducers'
 import './index.css';
 import App from './components/App';
-
-const store = createStore(rootReducer)
+import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/reducers/Reducer.js
@@ -0,0 +1,17 @@
+import { FETCH_MONSTERS, FETCH_ONE_MONSTER } from '../actions/types'
+
+const initialState = { 
+  monsters: [],
+  monster: {}
+}
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    case FETCH_MONSTERS:
+      return {...state, monsters: action.payload}
+    case FETCH_ONE_MONSTER:
+      return {...state, monster: action.payload}
+    default:
+      return state
+  }
+}
