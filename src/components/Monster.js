6f5ba16e769b50011d4373789b3140b694e2b96c
Elsa Fluss <elsafluss@gmail.com>
2021-02-24 09:46:20 -0700

    Display single monster's extended data from second API call.

--- src/components/Monster.js
+++ src/components/Monster.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { getOneMonster } from '../actions/actions'
-// make singleMonster api call here when the component mounts
 
 class Monster extends Component {
-    constructor() {
-        super()
-        this.state = {
-            deepMonster: {}
-        }
-    }
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         deepMonster: {}
+    //     }
+    // }
 
     componentDidMount() {
-      getOneMonster(this.props.location.state.url)
+        this.props.getOneMonster(this.props.location.state.url)
     }
-        
+    
     render() {
-        return (
-            <div>Monster<br></br>
-            {this.state.deepMonster.name}<br></br>
-            {this.state.deepMonster.charisma}<br></br>
-            {this.state.deepMonster.size}
-            </div>
-        )
+        if (this.props.monster.url === this.props.location.state.url) {
+            return (
+                <div>Monster<br></br>
+                    name {this.props.monster.name}<br></br>
+                    size {this.props.monster.size}<br></br>
+                    type {this.props.monster.type}<br></br>
+                    CR {this.props.monster.challenge_rating}<br></br><br></br>
+                    <button>click to add to encounter</button>
+                </div>
+            )
+        } else {
+            return null
+        }
     }
 }
-export default Monster
+
+const mapStateToProps = (state) => ({
+    monster: state.monsters.monster
+})
+
+export default connect(mapStateToProps, {getOneMonster})(Monster)
--- src/reducers/Reducer.js
+++ src/reducers/Reducer.js
@@ -5,6 +5,7 @@ const initialState = {
   monster: {}
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function(state = initialState, action) {
   switch(action.type) {
     case FETCH_MONSTERS:
