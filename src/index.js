0748435db5a509ccba667231c33a6b7d592a20a3
Drew Bradley <williamandrewb@gmail.com>
2021-02-23 19:30:29 -0700

    Install plethora of dependencies just to be able to write Redux code properly.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
 import './index.css';
 import App from './components/App';
 
+const store = createStore(rootReducer)
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
