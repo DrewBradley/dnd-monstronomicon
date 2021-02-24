7d86dfe7c00ed26ddb536b214ac1e1ccdfd7cf56
Elsa Fluss <elsafluss@gmail.com>
2021-02-23 17:28:33 -0700

    Create first API call.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Switch, NavLink, Link } from 'react-router-dom'
 import Display from './Display'
 import Monster from './Monster'
@@ -7,21 +7,45 @@ import Encounter from './Encounter'
 import './App.css';
 import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Display} />
-        <Route path="/encounter" component={Encounter} />
-        <Route path="/monster/" component={Monster} />
-      </Switch>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount() {
+    this.getMonsters()
+    
+  }
+  
+  
+  getMonsters() {
+    fetch('https://www.dnd5eapi.co/api/monsters')
+    .then(response => response.json())
+    .then(data => this.setState({ monsters: data.results }))
+    
+  }
+  
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Display} />
+          <Route path="/encounter" component={Encounter} />
+          <Route path="/monster/" component={Monster} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 App.propTypes = {
-  monster: PropTypes.object.isRequired,
+  // monster: PropTypes.object.isRequired,
 }
 
 export default App;
