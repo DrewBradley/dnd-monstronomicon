806b80b91d23bcf57b91b641b837bee78ca0a218
Drew Bradley <williamandrewb@gmail.com>
2021-02-24 18:46:51 -0700

    Refactor component to be class-based which fixed the context error.

--- src/components/ShowAllMonsters.js
+++ src/components/ShowAllMonsters.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { searchMonster } from '../actions/actions'
 import { connect } from 'react-redux'
 import './ShowAllMonsters.css'
 
-const ShowAllMonsters = () => {
-  const monsterList = this.props.monsters
-  return(
-    <button 
-      className="show-all-btn"
-      onClick={() => this.props.searchMonster(monsterList)}>
-      Show All
-    </button>
-  )
+class ShowAllMonsters extends Component {
+  render(){
+    const monsterList = this.props.monsters
+    return(
+      <button 
+        className="show-all-btn"
+        onClick={() => this.props.searchMonster(monsterList)}>
+        Show All
+      </button>
+    )
+  }
 }
 
 const mapStateToProps = (state) => ({
