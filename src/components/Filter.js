65bf96b3bc2675a93c33a2423ddee0f252789cfa
Elsa Fluss <elsafluss@gmail.com>
2021-02-24 17:34:38 -0700

    Display challenge rating numbers in dropdown list.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,15 @@
+/* eslint-disable react/jsx-pascal-case */
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Filter extends Component {
+    constructor() {
+        super()
+        this.state = {
+            challengeRating: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
 
 // dropdown list - selection sets CR variable
 // CR variable is passed through to the fetch call
@@ -9,24 +17,30 @@ class Filter extends Component {
 // then we update/render the new list of monsters in Display
 
     componentDidMount() {
+
     }
     
+    handleClick(event) {
+        event.preventDefault()
+        this.setState({ challengeRating: event.target.value })
+        console.log(event.target.value)
+    }
+
     render() {
-        const CRList = [0, .125, .25, .5, 1, 2,3]
-            var options = CRList.map(item => {
+        const CRList = ['select', 0, .125, .25, .5, 1, 2, 3, 4]
+            var opts = CRList.map((item, index) => {
                 return (
-                    <Control.select  key={item} text={item} value={item} />
+                    <option value={item} key={item}>{item}</option>
                     )
                 })
 
             return (
                 <>
-                    <div>Filter</div>
-                    <form onSubmit={this.props.handleClick}>
-                        <select name="CR" id="CR">
-                            {options}
-                        </select> 
-                        <button>filter by CR</button>
+                    <form onSubmit={this.handleClick}>
+                        <select>
+                            {opts}
+                        </select>
+                        <button>filter by challenge rating</button>
                     </form>
                 </>
             )
