8ca65f9d3b5b0b6d88ec8c44f9da83da828f0123
Elsa Fluss <elsafluss@gmail.com>
2021-02-25 18:59:39 -0700

    Add styling to single monster page display.

--- src/components/Monster.js
+++ src/components/Monster.js
@@ -24,24 +24,24 @@ class Monster extends Component {
         if (this.props.monster.url === this.props.location.state.url) {
             return (
                 <div className="monster-stats">
-                    <p className="one-stat">NAME: <span>{monster.name}</span></p>
-                    <p className="one-stat">TYPE: <span>{monster.type}</span></p>
-                    <p className="one-stat">SUBTYPE: <span>{monster.subtype}</span></p>
-                    <p className="one-stat">SIZE: <span>{monster.size}</span></p>
-                    <p className="one-stat">XP: <span>{monster.xp}</span></p>
-                    <p className="one-stat">ACTIONS: <span>{actions}</span></p>
-                    <p className="one-stat">ALIGNMENT: <span>{monster.alignment}</span></p>
-                    <p className="one-stat">ARMOR CLASS: <span>{monster.armor_class}</span></p>
-                    <p className="one-stat">CHALLENGE RATING: <span>{monster.challenge_rating}</span></p>
-                    <p className="one-stat">CHARISMA: <span>{monster.charisma}</span></p>
-                    <p className="one-stat">CONSTITUTION: <span>{monster.constitution}</span></p>
-                    <p className="one-stat">DEXTERITY: <span>{monster.dexterity}</span></p>
-                    <p className="one-stat">HIT DICE: <span>{monster.hit_dice}</span></p>
-                    <p className="one-stat">HIT POINTS: <span>{monster.hit_points}</span></p>
-                    <p className="one-stat">INTELLIGENCE: <span>{monster.intelligence}</span></p>
-                    <p className="one-stat">LANGUAGES: <span>{monster.languages}</span></p>
-                    <p className="one-stat">STRENGTH: <span>{monster.strength}</span></p>
-                    <p className="one-stat">WISDOM: <span>{monster.wisdom}</span></p>
+                    <p className="one-stat name">NAME: <span>{monster.name}</span></p>
+                    <p className="one-stat type">TYPE: <span>{monster.type}</span></p>
+                    <p className="one-stat subtype">SUBTYPE: <span>{monster.subtype}</span></p>
+                    <p className="one-stat size">SIZE: <span>{monster.size}</span></p>
+                    <p className="one-stat xp">XP: <span>{monster.xp}</span></p>
+                    <p className="one-stat actions">ACTIONS: <span>{actions}</span></p>
+                    <p className="one-stat alignment">ALIGNMENT: <span>{monster.alignment}</span></p>
+                    <p className="one-stat armor-class">ARMOR CLASS: <span>{monster.armor_class}</span></p>
+                    <p className="one-stat challenge-rating">CHALLENGE RATING: <span>{monster.challenge_rating}</span></p>
+                    <p className="one-stat charisma">CHARISMA: <span>{monster.charisma}</span></p>
+                    <p className="one-stat constitution">CONSTITUTION: <span>{monster.constitution}</span></p>
+                    <p className="one-stat dexterity">DEXTERITY: <span>{monster.dexterity}</span></p>
+                    <p className="one-stat hit-dice">HIT DICE: <span>{monster.hit_dice}</span></p>
+                    <p className="one-stat hit-points">HIT POINTS: <span>{monster.hit_points}</span></p>
+                    <p className="one-stat intelligence">INTELLIGENCE: <span>{monster.intelligence}</span></p>
+                    <p className="one-stat languages">LANGUAGES: <span>{monster.languages}</span></p>
+                    <p className="one-stat strength">STRENGTH: <span>{monster.strength}</span></p>
+                    <p className="one-stat wisdom">WISDOM: <span>{monster.wisdom}</span></p>
                     <br></br>
                     <button>click to add to encounter</button>
                 </div>
