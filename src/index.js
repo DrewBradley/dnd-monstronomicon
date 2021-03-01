10bdd2aa2f024ee42283fc10b854c73b71ed6780
Drew Bradley <williamandrewb@gmail.com>
2021-03-01 12:00:24 -0700

    Update service worker.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import './index.css';
 import App from './components/App';
 import store from './store'
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -14,3 +15,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register()
