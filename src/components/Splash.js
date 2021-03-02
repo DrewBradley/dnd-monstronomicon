a5e69293ac6aade94d9ea09cc7d8cb732e154a4f
Drew Bradley <williamandrewb@gmail.com>
2021-03-02 09:33:23 -0700

    Style Splash page and add Link to /home.

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -7,8 +7,10 @@ const Splash = () => {
         <div className="splash">
             <h1>Welcome to</h1>
             <h1 className="splash-title">MONSTRONOMICON</h1>
-            <Link to="/home" className="go-home">
-              Get Started
+            <Link to="/home" >
+              <p className="go-home">
+                Enter
+              </p>
             </Link>
         </div>
     )
