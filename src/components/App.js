01cea46b7a933c086318edd157f072810bee9a3e
Drew Bradley <williamandrewb@gmail.com>
2021-03-01 18:42:32 -0700

    Change home to be '/' and not '/home'.

--- src/components/App.js
+++ src/components/App.js
@@ -57,10 +57,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Switch>
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
-          <Route exact path="/home" render = {() => <Display monsterLinks={monsterLinks}/>}/>
+          <Route exact path="/" render = {() => <Display monsterLinks={monsterLinks}/>}/>
           <Route path="/encounter" component={Encounter} />
           <Route path="/monster/" component={Monster} />
         </Switch>
