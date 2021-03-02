9e14cea5770dcd732ccb933fdfdd93c00afc017f
Drew Bradley <williamandrewb@gmail.com>
2021-03-02 10:05:19 -0700

    Update main to push to GH Pages.

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Splash.css'
 import {Link} from 'react-router-dom'
 
-const Splash = () => {
-    return (
+class Splash extends Component {
+  render() {
+     return (
         <div className="splash">
             <h1>Welcome to</h1>
             <h1 className="splash-title">MONSTRONOMICON</h1>
@@ -14,6 +15,7 @@ const Splash = () => {
             </Link>
         </div>
     )
+  } 
 }
 
 export default Splash
